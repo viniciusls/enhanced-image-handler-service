90b4fab0f7f4bc5afb86882e29f18062fee50a58
Vinícius Silva <vinicius.ls@live.com>
2021-10-12 23:50:17 -0300

    feat: added handler base code

---
+++ handler/app.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk');
+const util = require('util');
+
+const sns = new AWS.SNS({apiVersion: '2010-03-31'});
+
+exports.handler = async (event, context, callback) => {
+  try {
+    // Read options from the event parameter.
+    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+    const srcBucket = event.Records[0].s3.bucket.name;
+
+    // Object key may have spaces or unicode non-ASCII characters.
+    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+    await publishToSNSTopic(srcBucket, srcKey);
+
+    console.log(`Successfully published ${srcBucket}/${srcKey} info to ${process.env.SNS_THUMBNAILS_TOPIC_ARN}`);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const publishToSNSTopic = async (bucketName, objectKey) => {
+  const messageBody = {
+    BucketName: bucketName,
+    ObjectKey: objectKey,
+    ObjectName: objectKey.split('/').pop(),
+    ObjectExtension: objectKey.split('.').pop()
+  };
+
+  const snsPublishParams = {
+    Message: {
+      default: messageBody,
+      sqs: messageBody
+    },
+    MessageStructure: 'json',
+    TopicArn: process.env.SNS_THUMBNAILS_TOPIC_ARN
+  };
+
+  const publishResult = await sns.publish(snsPublishParams).promise();
+  console.log(publishResult);
+}
