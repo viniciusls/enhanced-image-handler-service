831dad8c31ed015767be99a69bb812a39481526c
Vinícius Silva <vinicius.ls@live.com>
2021-10-12 23:29:13 -0300

    cleanup old code

--- main.tf
+++ main.tf
@@ -14,10 +14,3 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-/*module "s3-proxy-gateway" {
-  source      = "./s3-proxy-gateway"
-  environment = var.environment
-  region      = var.region
-} - DISABLED - TBD */
-
